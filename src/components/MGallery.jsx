eb1378c7d66f1a56a2b58f805ed0bb8d579c88d2
Chetan Verma <coutcv28@gmail.com>
2025-07-15 16:57:20 +0530

    Update MGallery.jsx

--- src/components/MGallery.jsx
+++ src/components/MGallery.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useRef, useContext, useEffect } from "react";
-import { FaArrowCircleLeft, FaArrowCircleRight, FaArrowLeft, FaDownload, FaExpand, FaShareAlt } from 'react-icons/fa'
-import { FaLeftLong } from "react-icons/fa6";
+import { FaArrowLeft } from 'react-icons/fa'
 import { view_Gallery_Context, view_individual_Flipbook_Context } from "../context/context";
 import { pdf_Context } from "../context/context";
+
+//importing pdf from public folder
 import pdf_1 from "/Comic_book_English.pdf"
 import pdf_2 from "/Comic_book_Hindi.pdf"
 import pdf_3 from "/comic_book.pdf"
@@ -18,14 +19,14 @@ const MGallery = () => {
         window.scrollTo(0, 0);
     }, []);
 
+
     const open_Book = (pdfName) => {
         pdf_Value.setPdf(pdfName)
         view_individual_Flipbook_Value.setView_individual_Flipbook(true)
     }
+
     return (
         <>
-
-
             <nav className='bg-[#4B9994] p-3 text-white font-bold text-center fixed w-full z-50 top-0'>
                 This is navbar for reference
             </nav>
@@ -37,24 +38,7 @@ const MGallery = () => {
                     <h1 className='text-3xl font-bold font-inter text-center'>Alumni Magazine Collection</h1>
                     <p className='mt-2 mx-auto text-center'>Celebrate inspiring journeys, nostalgic memories, and standout achievements from our global alumni. Each edition brings you closer to the community—one page at a time.</p>
                 </div>
-                {/* <div className="contentSection bg-[#B9CDC0] rounded-2xl mx-0 sm:mx-10  p-5 px-1 sm:px-10 mt-7 overflow-hidden">
-                    <div className='part1 flex justify-between'>
-                    <h1 className='font-bold text-3xl italic'>Magazine '25</h1>
-                    <div className='flex justify-between gap-2 sm:gap-5 list-none mt-3'>
-                    <FaExpand className='w-5 h-5 cursor-pointer transition-all transform hover:scale-120 duration-300 ease-in-out' onClick={()=>{}}/>
-                    <FaDownload className='w-5 h-5 cursor-pointer transition-all transform hover:scale-120 duration-300 ease-in-out' />
-                    <FaShareAlt className='w-5 h-5 cursor-pointer transition-all transform hover:scale-120 duration-300 ease-in-out' />
-                    </div>
-                    </div>
-                    <div className="flipbook flex justify-between items-center lg:gap-5 mt-5">
-                    <FaArrowCircleLeft className='hover:shadow-[0_0_10px_4px_rgba(120,120,120,0.5)] rounded-full  w-10 h-10 cursor-pointer transition-all transform hover:scale-110 duration-300 ease-in-out' onClick={() => flipBookRef.current?.pageFlip().flipPrev()}/>
-                    
-                    
-                    <FaArrowCircleRight className='hover:shadow-[0_0_10px_4px_rgba(120,120,120,0.5)] rounded-full w-10 h-10 cursor-pointer transition-all transform hover:scale-110 duration-300 ease-in-out' onClick={() => flipBookRef.current?.pageFlip().flipNext()}/>
-                    </div>
-                    <p className='underline w-fit mt-5 mx-auto cursor-pointer transition-all transform hover:scale-105 duration-300 ease-in-out'>View All Magazines</p>
-                    
-                    </div> */}
+
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-5 xl:mx-25 mt-5">
 
                     <div onClick={() => open_Book(pdf_1)} className="card bg-[#B9CDC0] m-2 p-5 w-fit rounded-2xl flex flex-col items-center justify-center cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-lg hover:shadow-black">
